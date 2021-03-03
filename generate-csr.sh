2c46949b3a6c09e2a70456bd3a11035ed8d9560f
Steven Harradine <stevenharradine@gmail.com>
2021-03-03 16:13:52 -0500

    make logging work on macs

--- generate-csr.sh
+++ generate-csr.sh
@@ -77,21 +77,21 @@ else
 	echo $password > $domain.password
 
 	echo -n "Generating private key for $domain"
-	openssl genrsa -aes256 -out $keyPath -passout pass:$password 2048 &>> $logPath
+	openssl genrsa -aes256 -out $keyPath -passout pass:$password 2048 >> $logPath 2>&1
 	echo " Done"
 
 	echo -n "Removing passphrase from private key"
 	cp $keyPath $keyorgPath > /dev/null
-	openssl rsa -in $keyorgPath -out $keyPath -passin pass:$password &>> $logPath
+	openssl rsa -in $keyorgPath -out $keyPath -passin pass:$password >> $logPath 2>&1
 	echo " Done"
 
 	echo -n "Generate certificate signing request (CSR)"
 	subject="/C=$country/ST=$state/L=$locality/O=$organisation/OU=$organisationalUnit/CN=$domain"
-	openssl req -new -key $keyPath -out $csrPath -subj "$subject" &>> $logPath
+	openssl req -new -key $keyPath -out $csrPath -subj "$subject" >> $logPath 2>&1
 	echo " Done"
 
 	echo -n "Generating encrypted zip"
-	7za a -tzip -p"$password" -mem=AES256 $zipPath $keyPath &>> $logPath
+	7za a -tzip -p"$password" -mem=AES256 $zipPath $keyPath >> $logPath 2>&1
 	echo " Done"
 
 	echo ""
